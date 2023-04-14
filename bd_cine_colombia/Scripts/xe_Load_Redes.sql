08613eda0608b976d03eb287ba4967907ea5888e
K-vin23 <kvin23@DESKTOP-C54U3M0.localdomain>
2023-04-14 15:47:02 -0500

    Firts Data Upload BD Cine Colombia

---
+++ bd_cine_colombia/Scripts/xe_Load_Redes.sql
@@ -0,0 +1,18 @@
+/*==============================================================*/
+/* Table: REDES                                                 */
+/*==============================================================*/
+/*create table REDES (
+   ID_RED               VARCHAR2(2)           not null,
+   RED_SOCIAL           VARCHAR2(50)          not null,
+   constraint PK_REDES primary key (ID_RED)
+);*/
+ 
+INSERT INTO REDES (ID_Red, Red_Social)VALUES ('In', 'Instagram');
+INSERT INTO REDES (ID_Red, Red_Social)VALUES ('Fa', 'Facebook');
+INSERT INTO REDES (ID_Red, Red_Social)VALUES ('Tw', 'Twitter');
+INSERT INTO REDES (ID_Red, Red_Social)VALUES ('Re', 'Reddit');
+INSERT INTO REDES (ID_Red, Red_Social)VALUES ('Pi', 'Pinterest');
+INSERT INTO REDES (ID_Red, Red_Social)VALUES ('Di', 'Discord');
+INSERT INTO REDES (ID_Red, Red_Social)VALUES ('Ti', 'TikTok');
+
+
